10f057fc04eef6ab552041f3226cd7edb3098166
Ana Bojanic <ana.bojanic@it-labs.com>
2023-03-25 15:28:44 +0100

    Initial commit

---
+++ LogAnalyzer/LogAnalyzer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string logFilePath = "C:\\Users\\ana.bojanic\\Documents\\ex040730.log";
+        List<string> logLines = File.ReadAllLines(logFilePath).ToList();
+
+        List<string> ipAddresses = logLines.Select(line => line.Split(' ')[0]).ToList();
+    
+        Dictionary<string, int> ipHitCounts = new Dictionary<string, int>();
+        foreach (string ip in ipAddresses)
+        {
+
+            if (ipHitCounts.ContainsKey(ip))
+            {
+                ipHitCounts[ip]++;
+            }
+            else
+            {
+                ipHitCounts[ip] = 1;
+            }
+        }
+
+        List<KeyValuePair<string, int>> sortedIPHits = ipHitCounts.ToList();
+        sortedIPHits.Sort((a, b) => b.Value.CompareTo(a.Value));
+    }
+}
